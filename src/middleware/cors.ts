2cce6c9db91474b6e02917894a78af3c2c37ae83
LeVien <levien.workspace@gmail.com>
2023-11-27 19:46:58 +0700

    cors

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,7 +2,7 @@ import cors from "@koa/cors";
 
 export const corsMiddleware = cors({
   origin: (ctx): string => {
-    const validDomains = ["http://localhost:8080", "https://transca.kitchen"];
+    const validDomains = ["http://localhost:8080", "https://transca.kitchen", "https://www.transca.kitchen"];
     if (validDomains.indexOf(ctx.request.header.origin!) !== -1) {
       return ctx.request.header.origin || "";
     }
